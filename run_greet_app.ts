63b1afcd26a9b88ed312a474322ac3d566feb574
tasawaa123 <86639082+tasawaa123@users.noreply.github.com>
2021-09-05 14:55:32 +0900

    Update run_greet_app.ts

--- run_greet_app.ts
+++ run_greet_app.ts
@@ -1,4 +1,18 @@
-import { createApp } from './create_greet_app.ts'
+import { Application, Router, helpers } from "https://deno.land/x/oak@v7.7.0/mod.ts";
+
+function createApp(): Application{
+    const app = new Application();
+    const router = new Router();
+
+    router.get("/greet", (ctx)=>{
+        const { name = "None" } = helpers.getQuery(ctx);
+        ctx.response.body = `Hello ${name}`;
+    });
+
+    app.use(router.routes());
+    return app;
+}
+
 
 const app = createApp();
 //await app.listen({port: 8888});
